c8f6f4c10e11758a667c00591003cb0760644501
Juan Páez <jdpluc302@gmail.com>
2023-04-18 11:24:30 -0500

    Autenticación por front funcionando

--- src/app/modules/autenticacion/login/login.component.ts
+++ src/app/modules/autenticacion/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import {AuthService} from "../../../services/auth.service";
+import {Router} from "@angular/router";
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -15,15 +17,25 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  email : String | undefined
-  contrasenia : String | undefined
+  constructor(private authService: AuthService, private router: Router)  {}
+  email : string = '';
+  contrasenia : string = '';
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   matcher = new MyErrorStateMatcher();
+  login(): void {
+    if (this.emailFormControl.invalid) {
+      alert("Correo no valido")
+      return;
+    }
 
-  login():void{
-    console.log(this.email)
-    console.log(this.contrasenia)
-    console.log("INICIO SESION")
-    
+    const message = this.authService.login(this.email, this.contrasenia);
+    if (message == 'Credenciales inválidas') {
+      alert(message);
+    } else {
+      // Si el inicio de sesión fue exitoso, navegar a la pantalla deseada
+      if (this.authService.isLogged) {
+        this.router.navigate(['/Equipos']);
+      }
+    }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,13 +9,29 @@ export class AuthService {
   isLogged = false;
   redirectUrl!: string;
 
-  constructor() { }
+  constructor() {
+  }
 
-  logout():void{
-    this.isLogged = false;
+  logout(): void {
+    this.isLogged = true;
 
   }
-  isLoggedIn():boolean{
+
+  isLoggedIn(): boolean {
     return this.isLogged;
+  }
+
+  login(username: string, password: string): string {
+
+    // Comprobar si las credenciales son válidas
+    if (username === 'a@a.es' && password === '1234') {
+      // Almacenar información del usuario en localStorage
+      localStorage.setItem('currentUser', JSON.stringify({username, password}));
+      // Establecer la bandera de inicio de sesión
+      this.isLogged = true;
+      return 'Inicio de sesión exitoso';
+    } else {
+      return 'Credenciales inválidas';
+    }
   }
 }
