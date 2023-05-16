dd53c1051e46842edd6a981359ca48e0c898c118
Kevyn Prada <koprada@javeriana.edu.co>
2023-05-15 19:56:08 -0500

    tablas solicitudes-administrador

--- src/app/modules/equipo/administrar-equipo/administrar-equipo.component.ts
+++ src/app/modules/equipo/administrar-equipo/administrar-equipo.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {SelectionModel} from '@angular/cdk/collections';
 import {MatTableDataSource} from '@angular/material/table';
+import { SolicitudService } from 'src/app/services/solicitud.service';
 import { MatCardModule } from '@angular/material/card';
 import { FormControl } from '@angular/forms';
 import * as _moment from 'moment';
 import {AuthService} from "../../../services/auth.service";
 import {Router} from "@angular/router";
+import { Solicitud } from 'src/app/shared/model/solicitud';
 
 const moment = _moment;
 export interface Item {
-  Nombre: string;
-  Edad: number;
+  username: string;
+  email: string;
 }
 @Component({
   selector: 'app-my-component',
@@ -18,28 +20,29 @@ export interface Item {
   styleUrls: ['./administrar-equipo.component.scss']
 })
 export class AdministrarEquipoComponent  implements OnInit{
+  constructor(private solicitudService: SolicitudService) { }
+  solicitudes:Solicitud[] = [];
   date = new FormControl(moment([2017, 0, 1]));
   date7: Date | undefined
+  items: Item[] = [];
+  displayedColumns: string[] = ['Nombre', 'Edad', 'select'];
+  dataSource!: MatTableDataSource<Item>;
+  selection = new SelectionModel<Item>(true, []);
   ngOnInit(): void {
-
+    const userId = JSON.parse(localStorage.getItem('currentUser')!).id;
+    this.solicitudService.getSolicitudes(userId).subscribe((solicitudes) => {
+      this.solicitudes = solicitudes;
+      solicitudes.forEach(Element => {
+        var obj = {
+          username: Element.usuario.username,
+          email: Element.usuario.email
+        }
+        this.items.push(obj);
+      })
+      this.dataSource = new MatTableDataSource<Item>(this.items);
+    })
   }
 
-  // Define las columnas a mostrar
-  displayedColumns: string[] = ['Nombre', 'Edad', 'select'];
-
-  // Define los items a mostrar
-  items: Item[] = [
-    {Nombre: 'Juan', Edad: 25},
-    {Nombre: 'Kevyn', Edad: 30},
-    {Nombre: 'Santiago', Edad: 20},
-    {Nombre: 'Mateo', Edad: 35},
-  ];
-
-  // Define la fuente de datos para la tabla
-  dataSource = new MatTableDataSource<Item>(this.items);
-
-  // Define el modelo de selección
-  selection = new SelectionModel<Item>(true, []);
 
   // Implementa el método para seleccionar/deseleccionar todos los items
   masterToggle() {
@@ -59,30 +62,30 @@ export class AdministrarEquipoComponent  implements OnInit{
   onAceptar() {
     if (this.selection.selected.length === this.dataSource.data.length) {
       console.log('Aceptado para todos los elementos seleccionados:');
-      this.selection.selected.forEach((row) => console.log(row.Nombre));
+      this.selection.selected.forEach((row) => console.log(row.username));
     } else {
       this.selection.selected.forEach(element => {
-        console.log(`Aceptado: ${element.Nombre}`);
+        console.log(`Aceptado: ${element.username}`);
       });
     }
   }
   onInvitar() {
     if (this.selection.selected.length === this.dataSource.data.length) {
       console.log('Invitados todos los elementos seleccionados:');
-      this.selection.selected.forEach((row) => console.log(row.Nombre));
+      this.selection.selected.forEach((row) => console.log(row.username));
     } else {
       this.selection.selected.forEach(element => {
-        console.log(`Invitado: ${element.Nombre}`);
+        console.log(`Invitado: ${element.username}`);
       });
     }
   }
   onRechazar() {
     if (this.selection.selected.length === this.dataSource.data.length) {
       console.log('Rechazado para todos los elementos seleccionados:');
-      this.selection.selected.forEach((row) => console.log(row.Nombre));
+      this.selection.selected.forEach((row) => console.log(row.username));
     } else {
       this.selection.selected.forEach(element => {
-        console.log(`Rechazado: ${element.Nombre}`);
+        console.log(`Rechazado: ${element.username}`);
       });
 
     }
