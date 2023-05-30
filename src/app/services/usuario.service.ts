c10e61a9b3e26a7e7a9ea8b6460fb59595d01c07
Juan Páez <jdpluc302@gmail.com>
2023-05-30 11:48:48 -0500

    Servicio traer usuario no en el equipo

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../shared/model/usuario';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
   constructor(private http: HttpClient){}
-  
+  getUsuariosNoEnEquipo(idEquipo: number): Observable<any> {
+    return this.http.get<User>(`${environment.backendAPI}/usuarios/noEnEquipo/${idEquipo}`);
+  }
 }
